11feafea9c32fa25a8dc81bd28ceb56566cc2c07
zhuyikai2002 <zhuyikai2002@gamil.com>
2025-12-30 23:16:38 +0800

    Site update: 2025-12-30 23:16:38

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,13 @@ hexo clean
 hexo g
 
 echo "🚚 [2/3] 发送成品网页到 VPS (SCP Upload)..."
-# ⚠️ 注意：如果你的 SSH 名字不是 myvps，请修改下面这行的 myvps
-scp -r public/* myvps:/var/www/html/
+# ⚠️ 注意：目标路径后面加了 blog/
+# 确保你已经在 VPS 上执行过 mkdir -p /var/www/html/blog
+scp -r public/* myvps:/var/www/html/blog/
 
 echo "💾 [3/3] 备份源码到 GitHub (Git Push)..."
 git add .
+# 这里的提交信息我保留了你的自动化时间戳
 git commit -m "Site update: $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
