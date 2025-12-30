0dcf56c7c4b6c4feacdb04f4247ec88a79a57e0f
zhuyikai2002 <zhuyikai2002@gamil.com>
2025-12-30 22:31:45 +0800

    Site update: 2025-12-30 22:31:45

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
-# 遇到错误立刻停止，防止连锁反应
 set -e
 
-echo "🚀 [1/3] 开始本地生产..."
+echo "🚀 [1/3] 开始本地生产 (Hexo Generate)..."
 hexo clean
 hexo g
 
-echo "🚚 [2/3] 发送成品网页到 VPS..."
-# 注意：这里的 myvps 必须和你 SSH Config 里的名字一致
+echo "🚚 [2/3] 发送成品网页到 VPS (SCP Upload)..."
+# ⚠️ 注意：如果你的 SSH 名字不是 myvps，请修改下面这行的 myvps
 scp -r public/* myvps:/var/www/html/
 
-echo "💾 [3/3] 备份源码到 GitHub..."
+echo "💾 [3/3] 备份源码到 GitHub (Git Push)..."
 git add .
-# 自动生成一个带时间的提交备注，显得很专业
 git commit -m "Site update: $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
